d6cdce20856cfbdd68855629e1a4452f4cc0f758
Lautaro Aguilar <aguilarlautaroexequiel867@gmail.com>
2023-09-16 16:42:48 -0300

    feat(admin): :sparkles: create modal components and providers

--- ecommerce-admin/app/layout.tsx
+++ ecommerce-admin/app/layout.tsx
@@ -1,13 +1,14 @@
-import "./globals.css";
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import { ClerkProvider } from "@clerk/nextjs";
+import './globals.css';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import { ClerkProvider } from '@clerk/nextjs';
+import { ModalProvider } from '@/providers/modal-provider';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "Admin Dashboard",
-  description: "Admin Dashboard",
+  title: 'Admin Dashboard',
+  description: 'Admin Dashboard',
 };
 
 export default function RootLayout({
@@ -17,8 +18,11 @@ export default function RootLayout({
 }) {
   return (
     <ClerkProvider>
-      <html lang="en">
-        <body className={inter.className}>{children}</body>
+      <html lang='en'>
+        <body className={inter.className}>
+          <ModalProvider />
+          {children}
+        </body>
       </html>
     </ClerkProvider>
   );
