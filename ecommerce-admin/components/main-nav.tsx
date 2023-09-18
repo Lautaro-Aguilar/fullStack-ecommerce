3244638f076c1ae36f751639a7ab53a8c7a07948
Lautaro Aguilar <aguilarlautaroexequiel867@gmail.com>
2023-09-17 22:16:57 -0300

    feat(admin): :sparkles: create settings page

--- ecommerce-admin/components/main-nav.tsx
+++ ecommerce-admin/components/main-nav.tsx
@@ -1,8 +1,8 @@
-'use client';
+"use client";
 
-import { cn } from '@/lib/utils';
-import Link from 'next/link';
-import { useParams, usePathname } from 'next/navigation';
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+import { useParams, usePathname } from "next/navigation";
 
 export default function MainNav({
   className,
@@ -14,21 +14,26 @@ export default function MainNav({
   const routes = [
     {
       href: `/${params.storeId}`,
-      label: 'Settings',
+      label: "Overview",
+      active: pathname === `/${params.storeId}`,
+    },
+    {
+      href: `/${params.storeId}/settings`,
+      label: "Settings",
       active: pathname === `/${params.storeId}/settings`,
     },
   ];
   return (
-    <nav className={cn('flex items-center space-x-4 lg:space-x-6', className)}>
+    <nav className={cn("flex items-center space-x-4 lg:space-x-6", className)}>
       {routes.map((route) => (
         <Link
           key={route.href}
           href={route.href}
           className={cn(
-            'text-sm font-medium transition-colors hover:text-primary',
+            "text-sm font-medium transition-colors hover:text-primary",
             route.active
-              ? 'text-black dark:text-white'
-              : 'text-muted-foreground'
+              ? "text-black dark:text-white"
+              : "text-muted-foreground"
           )}
         >
           {route.label}
