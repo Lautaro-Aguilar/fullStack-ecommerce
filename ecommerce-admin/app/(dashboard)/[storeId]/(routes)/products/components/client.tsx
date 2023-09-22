a200398187c8592aa05bb4284221b771d468c912
Lautaro Aguilar <aguilarlautaroexequiel867@gmail.com>
2023-09-21 21:15:13 -0300

    feat(admin): :sparkles: fetch data for products, categories, sizes and colors and modify titles & routes

--- ecommerce-admin/app/(dashboard)/[storeId]/(routes)/products/components/client.tsx
+++ ecommerce-admin/app/(dashboard)/[storeId]/(routes)/products/components/client.tsx
@@ -10,22 +10,22 @@ import { DataTable } from "@/components/ui/data-table";
 import { ApiList } from "@/components/ui/api-list";
 
 import { ProductColumn, columns } from "./columns";
-interface BillboardClientProps {
+interface ProductClientProps {
   data: ProductColumn[];
 }
 
-export const BillboardClient: React.FC<BillboardClientProps> = ({ data }) => {
+export const ProductClient: React.FC<ProductClientProps> = ({ data }) => {
   const router = useRouter();
   const params = useParams();
   return (
     <>
       <div className="flex items-center justify-between">
         <Heading
-          title={`Billboards (${data.length})`}
-          description="Manage billboards for your store"
+          title={`Products (${data.length})`}
+          description="Manage products for your store"
         />
         <Button
-          onClick={() => router.push(`/${params.storeId}/billboards/new`)}
+          onClick={() => router.push(`/${params.storeId}/products/new`)}
         >
           <Plus className="mr-2 h-4 w-4" />
           Add New
@@ -33,9 +33,9 @@ export const BillboardClient: React.FC<BillboardClientProps> = ({ data }) => {
       </div>
       <Separator />
       <DataTable searchKey="label" columns={columns} data={data} />
-      <Heading title="API" description="API calls for Billboards" />
+      <Heading title="API" description="API calls for Products" />
       <Separator />
-      <ApiList entityName="billboards" entityIdName="billboardId" />
+      <ApiList entityName="products" entityIdName="productId" />
     </>
   );
 };
