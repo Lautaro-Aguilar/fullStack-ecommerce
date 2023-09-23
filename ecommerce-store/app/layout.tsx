45aa54bb3cc3890e6c24833a4507c305b4d2a881
Lautaro Aguilar <aguilarlautaroexequiel867@gmail.com>
2023-09-23 13:10:28 -0300

    feat(admin): :tada: Setup project for store-client

---
+++ ecommerce-store/app/layout.tsx
@@ -0,0 +1,29 @@
+import { Urbanist } from "next/font/google";
+
+import Footer from "@/components/footer";
+import Navbar from "@/components/navbar";
+
+import "./globals.css";
+import type { Metadata } from "next";
+
+const urbanist = Urbanist({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Store",
+  description: "Store",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={urbanist.className}>
+        <Navbar />
+        {children} <Footer />
+      </body>
+    </html>
+  );
+}
